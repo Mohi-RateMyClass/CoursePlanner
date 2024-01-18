c0aec31942634090fe6d11b85a8b7f8d18fa437f
MaximilianWebb <max.t.webb@gmail.com>
2024-01-18 11:02:10 -0700

    hello wordl

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Component } from '@angular/core';
+import { DetailsComponent } from './details/details.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+const routeConfig: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home Page'
+    },
+    {
+        path: 'detail',
+        component: DetailsComponent,
+        title: 'Detail Page'
+    },
+    {
+        path: '*',
+        component: AppComponent,
+        title: 'Home Page'
+    }
+
+];
+
+export default routeConfig;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent,{
+  providers : [
+  provideRouter(routeConfig)
+]
+})
   .catch((err) => console.error(err));
