a86ee6922783b74c27c231f9b3175216e574a81b
MaximilianWebb <max.t.webb@gmail.com>
2024-01-23 11:20:18 -0700

    stuff

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient ,HttpClientModule} from '@angular/common/http';
+
+type classContents = {
+  name: string;
+  code: string;
+  credits: number;
+  length: number;
+  prereq : string;
+  subject : string;
+};
+
+
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
-export class DetailsComponent {
 
+export class DetailsComponent implements OnInit{
+    response;
+    constructor(private http: HttpClient){}
+
+    ngOnInit(){
+    
+
+    this.http.get<any>("https://sheets.googleapis.com/v4/spreadsheets/1uP9MF0Jf2FhqxcEToxJ014O2GpBNNhk04paK_6n4n7k/values/A1:F186").subscribe(data => {
+      this.response = data.total;
+    })
+    console.log(this.response)
+    }
 }
+
