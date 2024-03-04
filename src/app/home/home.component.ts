03a0131e500ac9a00852388c9975badaeff3e6e4
ParthChudappa <parth.chudappa@icloud.com>
2024-03-04 11:01:40 -0700

    API Problems

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GoogleSheetsService } from '../API/google-sheets.service';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  firstSlotData: any;
 
+  constructor(private googleSheetsService: GoogleSheetsService) { }
+
+  ngOnInit(): void {
+    this.googleSheetsService.getFirstSlotData().subscribe((data: any) => {
+      // Assuming the data structure is an array of values
+      // You may need to modify this based on your actual data structure
+      this.firstSlotData = data.values[0][0];
+    });
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    googleSheetsApiKey: 'AIzaSyAuDj2LWE4NZilljd0UC6UtMRUlbcVHDsI',
+    characters: {
+      spreadsheetId: '1xi_l_haPT-hvDdzLPQXztoUI7KmIqVTlXWJY85kBeV0',
+      worksheetName: 'apiTest'
+    }
+  };
